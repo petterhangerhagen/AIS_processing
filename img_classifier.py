ebd8fb845174d74eebb06ef049793d67d343068a
Inger Berge Hagen <inger.b.hagen@ntnu.no>
2022-04-20 16:18:25 +0200

    Clean up code img_classifier.py

--- img_classifier.py
+++ img_classifier.py
@@ -1,12 +1,12 @@
-""" A tool to manually classify situations                      """
-""" Use auto_plot to generate images first                      """
+import cv2  # Import the OpenCV library
+import pandas as pd  # Import Pandas library
+import sys  # Enables the passing of arguments
+""" A tool to manually classify situations
+Use auto_plot to generate images before running this script
 
-""" Requirements                                                """
-""" -OpenCV (used in conjunction with conda and python 3.6.13)  """
-import cv2 # Import the OpenCV library
-import numpy as np # Import Numpy library
-import pandas as pd # Import Pandas library
-import sys # Enables the passing of arguments
+Requirements:
+- OpenCV (used in conjunction with conda and python 3.6.13)  """
+
 
 overwrite = False 
 
@@ -17,7 +17,7 @@ except:
     print("Assuming name of file: ", csv_file)
 
 
-df = pd.read_csv(csv_file, sep = ';') 
+df = pd.read_csv(csv_file, sep=';')
 print(df)
 
 if 'img_class' not in df:
@@ -37,12 +37,12 @@ print("Controls \nt: True \nf: False \nu: Undo previous classification \nq: quit
 for i, INPUT_IMAGE in enumerate(list_of_files):
     if pd.isnull(INPUT_IMAGE):
         continue
-    print("Nr:",i," ",INPUT_IMAGE, str(img_class[i]))
+    print("Nr:", i, " ", INPUT_IMAGE, str(img_class[i]))
     if str(img_class[i]) != 'nan' and not overwrite:
         continue
     image = cv2.imread(INPUT_IMAGE, -1)
 
-    quit = False
+    quit_viewing = False
     img_done = False
 
     while True:
@@ -52,11 +52,11 @@ for i, INPUT_IMAGE in enumerate(list_of_files):
         # Continue until 'q' is pressed:
         k = cv2.waitKey(0)
 
-        if k == 27:#esc
+        if k == 27:  # esc
             exit()
 
         if k == ord('q'):
-            quit = True 
+            quit_viewing = True
             img_done = True
 
         if k == ord('t'):
@@ -75,8 +75,7 @@ for i, INPUT_IMAGE in enumerate(list_of_files):
             cv2.destroyAllWindows()
             break
 
-
-    if quit:
+    if quit_viewing:
         cv2.destroyAllWindows()
         break
 
@@ -85,6 +84,6 @@ df['img_class'] = img_class
 
 print(df)
  
-df.to_csv(csv_file, index = None, sep = ';') 
+df.to_csv(csv_file, index=None, sep=';')
  
 exit()
