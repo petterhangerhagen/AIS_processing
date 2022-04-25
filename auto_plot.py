b50272ae2d94620a47b7a7696c6f53c171f101bc
Inger Berge Hagen <inger.b.hagen@ntnu.no>
2022-04-25 16:44:14 +0200

    Make auto_plot.py use parameter and ais files for plotting instead of recalculating them using AutoVerification instance@

--- auto_plot.py
+++ auto_plot.py
@@ -4,6 +4,10 @@ import time
 import pandas as pd
 import numpy as np
 import multiprocessing as mp
+from matplotlib import pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from datetime import datetime, date, time
+from AV_class_functions.helper_methods import knots_to_mps
 
 """ Runs AutoVerify on every case in folders of 'paths'    
 All outputs will be saved in /para/ folder                  
@@ -23,7 +27,6 @@ paths = ['./encs_selected', './encs_north', './encs_south']
 # ---------------------------------------------------------------------------------------------------
 # Combined algorithm
 def get_case_param_from_file(filename, specific_own_name, specific_obst_names, man_idx, q, df_row):
-
     if not multiple:
         print(filename)
 
@@ -56,7 +59,7 @@ def get_case_param_from_file(filename, specific_own_name, specific_obst_names, m
                     sit_happened = True
                 if k == 0:
                     continue
-                if av.situation_matrix[vessel.id, obst.id, k] != av.situation_matrix[vessel.id, obst.id, k-1]:
+                if av.situation_matrix[vessel.id, obst.id, k] != av.situation_matrix[vessel.id, obst.id, k - 1]:
                     sits.append(k)
             if sit_happened:
                 av.filterOutNonCompleteSituations(vessel, obst)
@@ -79,13 +82,140 @@ def get_case_param_from_file(filename, specific_own_name, specific_obst_names, m
 
             av.OWN_SHIP = vessel.id
             try:
-                q.put((av.plot_trajectories2(show_trajectory_at=man_idx, specific_obst_names=specific_obst_names,
-                                             save=True, save_folder=save_folder), df_row))
+                q.put((av.plot_trajectories(show_trajectory_at=man_idx, specific_obst_names=specific_obst_names,
+                                            save=True, save_folder=save_folder), df_row))
             except Exception as ex:
                 print(ex)
 
 
+def get_params_and_plot(para_df, os_df, tg_df, q, df_row):
+    try:
+        q.put((plot_situation(para_df, os_df, tg_df), df_row))
+    except Exception as ex:
+        print(ex)
+
+
+def plot_situation(para_df, os_df, tg_df):
+    """Plot single situation from dataframes containing AIS data and extracted parameters.
+    The plots include:
+    * start and stop of COLREGS situation
+    * start point of the maneuver registered in the parameter file
+    * cpa
+    :param para_df: Dataframe containing parameters extracted from the situation
+    :param os_df: Dataframe containing AIS data from vessel considered the ownship
+    :param tg_df: Dataframe containing AIS data from vessel considered the obstacle"""
+    # TODO: Plot cpa when the index has been added to the parameter file
+    # TODO: Plot trajectory prediction at maneuver stop
+
+    x_min = 9.0 * 10 ** 9
+    x_max = -9.0 * 10 ** 9
+    y_min = x_min
+    y_max = x_max
+    x_min_temp = min(min(os_df['lon'].values), min(tg_df['lon'].values))
+    x_max_temp = max(max(os_df['lon'].values), max(tg_df['lon'].values))
+    x_min = min(x_min, x_min_temp)
+    x_max = max(x_max, x_max_temp)
+    y_min_temp = min(min(os_df['lat'].values), min(tg_df['lat'].values))
+    y_max_temp = max(max(os_df['lat'].values), max(tg_df['lat'].values))
+    y_min = min(y_min, y_min_temp)
+    y_max = max(y_max, y_max_temp)
+    margin = 0.02
+    lon_bounds = [x_min - margin, x_max + margin]
+    lat_bounds = [y_min - margin, y_max + margin]
+    maneuver_start = int(para_df['maneuver_index_own'].values[0])
+    sit_start = para_df['start_idx'].values[0]
+    sit_stop = para_df['stop_idx'].values[0]
+
+    fig, ax = plt.subplots()
+    # Prepare mapping
+    mapping = Basemap(projection='merc',
+                      lat_0=lat_bounds[0], lon_0=lon_bounds[0],
+                      llcrnrlat=lat_bounds[0], urcrnrlat=lat_bounds[1],
+                      llcrnrlon=lon_bounds[0], urcrnrlon=lon_bounds[1], resolution='h', ax=ax)
+    mapping.drawcoastlines(linewidth=0.25)
+    mapping.fillcontinents(color='coral', lake_color='aqua')
+    # Map trajectories to chosen projection
+    os_x, os_y = mapping(os_df['lon'].values, os_df['lat'].values)
+    tg_x, tg_y = mapping(tg_df['lon'].values, tg_df['lat'].values)
+
+    # Plot trajectories
+    mapping.plot(os_x, os_y, color='b', label='ownship')
+    mapping.plot(tg_x, tg_y, color='r', label='obstacle')
+
+    # Mark significant indices
+    mapping.scatter(os_x[0], os_y[0], color='b', marker='x', label='start')
+    mapping.scatter(tg_x[0], tg_y[0], color='r', marker='x')
+
+    mapping.scatter(os_x[sit_start], os_y[sit_start], color='g', marker='d')
+    mapping.scatter(tg_x[sit_start], tg_y[sit_start], color='g', marker='d')
+    ax.annotate('COLREG\nstart', (os_x[sit_start], os_y[sit_start]), xytext=(os_x[sit_start] + 5, os_y[sit_start] + 5))
+    mapping.scatter(os_x[sit_stop], os_y[sit_stop], color='y', marker='d')
+    mapping.scatter(tg_x[sit_stop], tg_y[sit_stop], color='y', marker='d')
+    ax.annotate('COLREG\nstop', (os_x[sit_stop], os_y[sit_stop]), xytext=(os_x[sit_stop] + 5, os_y[sit_stop] + 5))
+
+    if para_df['maneuver_made_own'].values[0]:
+        # Plot predicted trajectory at index before maneuver
+        if isinstance(param_df['time'].values[0], str):
+            time_delta = datetime.strptime(param_df['time'].values[0], "%H:%M:%S") - datetime(1900, 1, 1)
+        else:  # Assuming datetime.time
+            time_delta = datetime.combine(date.min, param_df['time'].values[0]) - datetime.min  # if 'time' formatted as
+        dt = time_delta.total_seconds() / (param_df['stop_idx'].values[0] - param_df['start_idx'].values[0])
+        n_msgs = len(os_x)
+        speed = knots_to_mps(os_df['sog'].values[maneuver_start - 1])
+        course = os_df['cog'].values[maneuver_start - 1]
+        pred_traj = predict_trajectory(os_df['lon'].values, os_df['lat'].values, maneuver_start - 1,
+                                       n_msgs, dt, speed, course)
+        pred_x, pred_y = mapping(pred_traj[0, :], pred_traj[1, :])
+        mapping.plot(pred_x, pred_y, color='k', linestyle=':', label='prediction')
+
+        mapping.scatter(os_x[maneuver_start], os_y[maneuver_start], color='b', marker='o', label='maneuver')
+        title_string = ''
+    else:
+        title_string = '\n no evasive maneuver'
+    ax.legend()
+    title_string = str(para_df['own_name'].values[0]) + ' - ' + str(para_df['obst_name'].values[0]) + title_string
+    ax.set_title(title_string)
+
+    fig.set_size_inches(10.4, 8.8)
+    plt.show()
+
+
+def predict_trajectory(lon, lat, index, n_msgs, dt, speed, course):
+    """
+    Predict a straight trajectory for a vessel at given index.
+    :param lon: Longitudinal positions of original trajectory samples.
+    :param lat: Latitudinal positions of original trajectory samples.
+    :param index: Sample index where to start prediction.
+    :param n_msgs: Number of samples in original trajectory.
+    :param dt: Sample interval.
+    :param speed: Speed of vessel at index in meters per second
+    :param course: Course angle at index in degrees.
+    """
+
+    curr_pos = np.array([lon[index], lat[index]])
+
+    if course < 0:
+        course += 360
+
+    n_steps = n_msgs - index + 1
+    step = 0
+    trajectory = np.empty([2, n_steps])
+    trajectory[:, 0] = curr_pos
+
+    for k in range(1, n_steps):
+        step += 1
+        distance = speed * step * dt
+
+        trajectory[0, k] = lon[index] + np.sin(np.deg2rad(course)) * distance * 360 / (
+                    6362.132 * 1000 * np.pi * 2 * np.cos(np.deg2rad(lat[index])))
+        trajectory[1, k] = lat[index] + np.cos(np.deg2rad(course)) * distance / 111040
+
+    return trajectory
+
+
 #################################################################################
+
+
 if __name__ == '__main__':
 
     def save2dataframe(name, df, df_row):
@@ -93,7 +223,8 @@ if __name__ == '__main__':
             df['img_name'] = ""
         df['img_name'][df_row] = name
         return df
-    
+
+
     def write2csv(df):
         df.to_csv(csv_file_name.replace(".csv", "_img.csv"), sep=';')
         return
@@ -113,35 +244,54 @@ if __name__ == '__main__':
         import sys
         import psutil
 
-        random.shuffle(dirs)
-        random.shuffle(files)
+        random.shuffle(dirs)  # Why is this shuffle necessary?
+        random.shuffle(files)  # Why is this shuffle necessary?
         number_of_files = len(files)
         for count, file_name in enumerate(files):
             if file_name.endswith("60-sec.csv"):
-                
-                filename_code = file_name.replace("-60-sec.csv", "")[-5:]
-                df_read_c = df_read[df_read['case'] == filename_code]
 
-                if len(df_read_c) != 0:
+                # filename_code = file_name.replace("-60-sec.csv", "")[-5:]  # filename must be ais data
+                start = ' - '
+                end = '-60-sec'
+                filename_code = file_name[file_name.find(start) + len(start):file_name.rfind(end)][-5:]
+                param_df = df_read[df_read['case'] == filename_code]  # Parameter data
+
+                # # If it doesn't work to use the case_path variable, try this:
+                # for ais_root, ais_directory, ais_files in os.walk(root):
+                #     for ais_file in ais_files:
+                #         if ais_file.endswith(".csv") and (filename_code + '-60-sec' in ais_file):
+                #             case_path = os.path.join(ais_root, ais_file)
+                #             break
+                case_path = os.path.join(root, file_name)
+                ais_df = pd.read_csv(case_path, sep=';')  # AIS data
+
+                # TODO: Write plotting functions to repplace get_case_param_from_file and av.plot_trajectories to avoid
+                #  creation of Autoverification instance
+                if len(param_df) != 0:
                     if not multiple:
-                        print(df_read_c)
+                        print(param_df)
                 else:
                     continue
 
-                for i in range(len(df_read_c)):
-                    own_name = df_read_c.own_name.tolist()[i]
-                    obst_name = [df_read_c.obst_name.tolist()[i]]
-                    maneuver_idx = df_read_c.maneuver_index_own.tolist()[i]
-                    row = df_read_c.index.tolist()[i]
+                for i in range(len(param_df)):
+                    own_name = param_df.own_name.tolist()[i]
+                    obst_name = [param_df.obst_name.tolist()[i]]
+                    maneuver_idx = param_df.maneuver_index_own.tolist()[i]
+                    row = param_df.index.tolist()[i]
 
-                    if 'img_name' in df_read: 
+                    ownship_df = ais_df.loc[ais_df['own_name'] == own_name]
+                    obst_df = ais_df.loc[ais_df['obst_name'] == obst_name]
+                    sit_df = param_df[row]
+
+                    if 'img_name' in df_read:
                         val = df_read.at[row, 'img_name']
                         if isinstance(val, str):
                             if val != '':
                                 continue
 
                     if not multiple:
-                        get_case_param_from_file(file_name, own_name, obst_name, maneuver_idx, queue, row)
+                        # get_case_param_from_file(file_name, own_name, obst_name, maneuver_idx, queue, row)
+                        get_params_and_plot(sit_df, ownship_df, obst_df, queue, row)
                         while not queue.empty():
                             img_name, row = queue.get()
                             df_read = save2dataframe(img_name, df_read, row)
@@ -150,19 +300,21 @@ if __name__ == '__main__':
 
                         continue
 
-                    p = mp.Process(target=get_case_param_from_file,
-                                   args=(file_name, own_name, obst_name, maneuver_idx, queue, row,))
+                    # p = mp.Process(target=get_case_param_from_file,
+                    #                args=(file_name, own_name, obst_name, maneuver_idx, queue, row,))
+                    p = mp.Process(target=get_params_and_plot,
+                                   args=(sit_df, ownship_df, obst_df, queue, row,))
                     proc.append(p)
                     p.start()
 
                     sys.stdout.flush()
                     sys.stdout.write("Working file %s/%s - children working %s. CPU percent %s      \r " % (
-                                     count, number_of_files, len(proc), psutil.cpu_percent(interval=0.2)))
+                        count, number_of_files, len(proc), psutil.cpu_percent(interval=0.2)))
 
                     while psutil.cpu_percent(interval=0.2) > cpu_usage:
                         sys.stdout.flush()
                         sys.stdout.write("Working file %s/%s - children working %s. CPU percent %s.      \r " % (
-                                         count, number_of_files, len(proc), psutil.cpu_percent(interval=0.2)))
+                            count, number_of_files, len(proc), psutil.cpu_percent(interval=0.2)))
                         for ps in proc:
                             ps.join(timeout=0)
                             if not ps.is_alive():
@@ -190,4 +342,3 @@ if __name__ == '__main__':
         df_read = save2dataframe(img_name, df_read, row)
 
     write2csv(df_read)
-
